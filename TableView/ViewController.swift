b93925ba4f65f88fdf500ddf8781f837ee75d3fd
Aryan-iimjobs.com <aryan@iimjjobs.com>
2020-02-11 11:28:19 +0530

    separated table functions into extension

--- TableView/ViewController.swift
+++ TableView/ViewController.swift
@@ -8,54 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class ViewController: UIViewController {
     
     let cellSpacingHeight: CGFloat = 5
     var selectedVideo:Video?
     
-    func numberOfSections(in tableView: UITableView) -> Int {
-        videos.count
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
-    }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return self.view.frame.size.width / 2
-    }
-    
-    // Make the background color show through
-    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        let headerView = UIView()
-        headerView.backgroundColor = UIColor.clear
-        return headerView
-    }
-    
-    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return cellSpacingHeight
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        selectedVideo = videos[indexPath.section]
-        
-        self.performSegue(withIdentifier: "goToInfo", sender: self)
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
-        
-        let label = cell.viewWithTag(2) as! UILabel
-        
-        label.text = videos[indexPath.section].title
-        
-        cell.backgroundColor = UIColor.white
-        cell.layer.cornerRadius = 1
-        cell.clipsToBounds = true
-        
-        return cell
-    }
+   
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
@@ -78,3 +36,49 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 
 }
 
+extension ViewController : UITableViewDelegate,UITableViewDataSource{
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+           videos.count
+       }
+       
+       func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+           return 1
+       }
+       
+       func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+           return self.view.frame.size.width / 2
+       }
+       
+       // Make the background color show through
+       func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+           let headerView = UIView()
+           headerView.backgroundColor = UIColor.clear
+           return headerView
+       }
+       
+       func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+           return cellSpacingHeight
+       }
+       
+       func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+           selectedVideo = videos[indexPath.section]
+           
+           self.performSegue(withIdentifier: "goToInfo", sender: self)
+       }
+       
+       func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+           
+           let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+           
+           let label = cell.viewWithTag(2) as! UILabel
+           
+           label.text = videos[indexPath.section].title
+           
+           cell.backgroundColor = UIColor.white
+           cell.layer.cornerRadius = 1
+           cell.clipsToBounds = true
+           
+           return cell
+       }
+}
